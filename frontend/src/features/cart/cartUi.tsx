c44abb420d860ea371103394fda7a69d64219604
lukakhaja <lukakhaja@yahoo.com>
2026-06-04 23:25:54 +0400

    bitasmbl add: Implement cart management UI

--- frontend/src/features/cart/cartUi.tsx
+++ frontend/src/features/cart/cartUi.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function catalog() {
   return (
     <div className="bg-red">
-      <h1 className="text-white">This is big cart, yoo</h1>
+      <h1 className="text-white">This is big cart, yoo, barabum</h1>
     </div>
   );
 }
